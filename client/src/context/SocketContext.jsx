4b2b26ecab68436a719f79ee0142f84ce6e33d2b
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-12-29 19:22:07 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useCallback, useEffect, useMemo } from "react"
 import { toast } from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BACKEND_URL = "http://localhost:3000/";
+const BACKEND_URL = "https://squadx.onrender.com/";
 
 
 const SocketContext = createContext()
